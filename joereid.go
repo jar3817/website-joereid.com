44317d57b6fc273cf0502229dedd153da524971b
Joe Reid <joe@joereid.com>
2021-07-23 15:40:59 -0400

    Added logging

--- joereid.go
+++ joereid.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 	"text/template"
@@ -26,9 +27,18 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 	t, _ := template.ParseFiles("template/index.tmpl")
 	t.Execute(w, data)
+
+	log.Println(r.Method, r.RemoteAddr, r.URL.Path, r.UserAgent())
 }
 
 func main() {
+	logfile, err := os.OpenFile("joereid.com.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	log.SetOutput(logfile)
 	r := mux.NewRouter()
 
 	// catch any requests for "real" files (css, js, images, etc)
